2146e0dd167325b96c5629133a1f3e813c9f1e32
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-08-29 01:55:20 +0600

    done search section

--- script/phone.js
+++ script/phone.js
@@ -1,5 +1,5 @@
-const phoneLoad = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+const phoneLoad = async (search) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`);
     const data = await res.json();
     const phones = data.data
     console.log(phones);
@@ -9,10 +9,22 @@ phoneLoad()
 
 const displayPhone = phones => {
     const phoneContainer = document.getElementById('phone-container')
+    // search before clear 
+    phoneContainer.textContent = '';
+    // show all btn 
+    const showAll = document.getElementById('show-all');
+    if (phones.length>18){
+        showAll.classList.remove('hidden');
+    }
+    else{
+        showAll.classList.add('hidden')
+    }
+    // display only 20
+    phones = phones.slice(0,15);
     phones.forEach(phone => {
         console.log(phone)
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card card-compact w-96 bg-base-100 shadow-xl"`;
+        phoneCard.classList = `card card-compact bg-base-100 shadow-xl"`;
         phoneCard.innerHTML = `
         <figure>
               <img
@@ -20,14 +32,28 @@ const displayPhone = phones => {
                 alt="Shoes"
               />
             </figure>
-            <div class="card-body">
-              <h2 class="card-title">${phone.phone_name}</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div class="card-actions justify-end">
+            <div class="card-body justify-center">
+              <h2 class="card-title justify-center ">${phone.phone_name}</h2>
+              <p class="text-center">If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-center">
                 <button class="btn btn-primary">Buy Now</button>
               </div>
             </div>
         `
         phoneContainer.appendChild(phoneCard)
     });
+}
+
+const searchHandle =()=>{
+    loading();
+    const searchField = document.getElementById('search');
+    searchField.textContent = '';
+    const searchValue = searchField.value;
+    console.log(searchValue);
+    phoneLoad(searchValue);
+}
+
+const loading = ()=>{
+    const loading = document.getElementById("loading");
+    loading.classList.remove('hidden')
 }
